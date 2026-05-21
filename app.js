85ebccdd5917daca0b94abf6176efc3231d58a77
Jay Jadav <jayjadav9313@gmail.com>
2026-05-21 12:59:32 +0530

    add

--- app.js
+++ app.js
@@ -99,4 +99,8 @@ function isLoggedin(req, res, next) {
   }
 }
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
