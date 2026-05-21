857e980307b16b9c71e113222f22e2942b39d5b4
Jay Jadav <jayjadav9313@gmail.com>
2026-05-21 14:54:08 +0530

    recerse

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,17 +1,9 @@
 const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
 
-// console.log("Connecting to:", process.env.MONGODB_URI); // 👈 temporary debug
-
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => {
-    // console.log("MongoDB connected ✅"); // 👈 visible in Render logs
-    dbgr("connected");
-  })
-  .catch((err) => {
-    // console.error("MongoDB connection error ❌", err.message); // 👈 visible in Render logs
-    dbgr(err);
-  });
+  .then(() => dbgr("connected"))
+  .catch((err) => dbgr(err));
 
 module.exports = mongoose.connection;
